f034f2a977ea6f9a7477875cccd0a6bf24c9d47c
eddypnup <eddy.tungadi@poliupg.ac.id>
2021-05-18 01:25:01 +0800

    first commit

---
+++ application/views/nav.php
@@ -0,0 +1,41 @@
+<!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: #ffff">
+      <div class="container">
+        <a class="navbar-brand" style="font-family: Neutral Face;" href="#">MUNDANO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#women">WOMEN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#men">MEN</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                COLLECTION
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#">FW20</a></li>
+                <li><a class="dropdown-item" href="#">SS20</a></li>
+                <li><a class="dropdown-item" href="#">FW19</a></li>
+                <li><a class="dropdown-item" href="#">SS19</a></li>
+              </ul>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="login.html">ACCOUNT</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">CART (0)</a>
+            </li>
+            
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar end -->
